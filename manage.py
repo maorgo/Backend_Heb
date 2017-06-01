c205f0f40a338aee115a2f013dfd4bdb801e702a
maor-goaz <maor.goaz@hpe.com>
2017-06-01 14:18:20 +0300

    upgrade to flask_migrate / flask_script

--- manage.py
+++ manage.py
@@ -1,5 +1,7 @@
-from flask.ext.script import Manager
-from flask.ext.migrate import Migrate, MigrateCommand
+# from flask.ext.script import Manager
+# from flask.ext.migrate import Migrate, MigrateCommand
+from flask_script import Manager
+from flask_migrate import Migrate, MigrateCommand
 from Ranch import app, db
 
 
